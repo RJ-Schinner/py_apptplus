c8a1fd8aeff5ed0828086a217464fd446671fd5d
hydrohd <eaescob3@asu.edu>
2022-03-09 12:15:26 -0700

    customers test

---
+++ customers_test.py
@@ -0,0 +1,14 @@
+from py_apptplus.customers import *
+import os
+
+
+creds = {
+    'user': os.environ.get('APPT_PLUS_USER'),
+    'pass': os.environ.get('APPT_PLUS_PASS'),
+    'baseURL': 'https://ws.appointment-plus.com'
+}
+
+CUSTV1 = CustomersV1(creds)
+customer = CUSTV1.getCustomer(33079)
+
+print(customer.email)
