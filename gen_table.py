fec3cfe382bc2b1e4d755cd84a3f04af3c3599a3
hydrohd <eaescob3@asu.edu>
2022-04-01 13:18:19 -0700

    gen_table script changed for filtering po

--- gen_table.py
+++ gen_table.py
@@ -15,16 +15,21 @@ now = datetime.now()
 start = now - timedelta(days=30)
 end = now + timedelta(days=150)
 appts = APTSV1.getAppointmentsInRange(start, end)
+filteredAppts = [
+    apt for apt in appts
+    if apt.poNumber.upper()[0] == 'P'
+    and len(apt.poNumber) == 8
+]
 
 data = [{
     'location_id': apt.locationID,
     'customer_id': apt.customerID,
     'start_time': apt.startTime,
     'end_time': apt.endTime,
-    'po_number': apt.poNumber,
+    'po_number': apt.poNumber.upper(),
     'status_desc': apt.statusDesc
 
-} for apt in appts]
+} for apt in filteredAppts]
 
 df = pd.DataFrame(data)
 
